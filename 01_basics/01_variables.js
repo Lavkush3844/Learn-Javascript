8056f179e7144650eca034ba832768c8962ac559
Lavkush Varma <140130429+Lavkush3844@users.noreply.github.com>
2025-05-05 07:30:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 1234;
+let accountName = "Lavkush Varma";
+var accountCity = "Surat";
+accountEmail = "lavkush3844@gmail.com";
+
+accountName = "Varma";
+accountCity = "Ayodhya";
+accountEmail = "lavkush@gmail.com";
+
+/*
+    Prefer not to use var
+    because of issue in block scope and functional scope
+*/
+
+console.table([accountID, accountName, accountCity, accountEmail]);
