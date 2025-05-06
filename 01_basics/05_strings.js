509d382f189d23f14a819475da5a0cd6b59eaa50
lavkush3844 <lavkush3844@gmail.com>
2025-05-06 16:10:06 +0530

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = "lavkush"
+const repoCount = 15
+// console.log(name + repoCount);
+// console.log(`hello my name is  ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('bgmi-pubg')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('i'))
+
+const newString = gameName.substring(0, 1);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 0);
+
+const newStringOne = "   Lavkush    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://lk.com/lk%20varma"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('lk'))
+// console.log(url.includes('dsfsdf'))
+
+console.log(gameName.split('-'));
+
