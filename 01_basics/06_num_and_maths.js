1c0691d2655bfb895b605f9be7fe6f89bc7acea1
lavkush3844 <lavkush3844@gmail.com>
2025-05-07 10:41:25 +0530

    num_and_maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,29 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100.2565454)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(4, 3, 6, 1));
+// console.log(Math.max(4, 3, 6, 1));
+
+// console.log((Math.random() * 10) + 1);
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
